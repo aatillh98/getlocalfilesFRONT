48f5896ec01fabbf91cc99893a7812e9de3d7d8f
aatillh98 <woowanass@gmail.com>
2023-12-05 23:26:44 +0100

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,67 @@
+<!-- DirectoryListing.vue -->
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <b-container fluid="md">
+      <b-row>
+        <b-col cols="8">
+          <b-form-input v-model="directory" placeholder="Enter your name"></b-form-input>
+        </b-col>
+        <b-col>
+          <b-button @click="getDirectoryListing" variant="success" :disabled="disabledButton">Get Listing</b-button>
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+      <b-row class="mt-4">
+        <b-col></b-col>
+        <b-col>
+          <b-list-group v-for="item in listing" :key="item">
+        <b-list-group-item>{{ item }}</b-list-group-item>
+      </b-list-group>
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { BFormInput, BListGroup, BButton, BCol, BRow, BContainer, BListGroupItem } from 'bootstrap-vue'
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  components: {
+    BFormInput,BListGroup,BButton, BCol, BRow, BContainer, BListGroupItem
+  },
+  data() {
+    return {
+      directory: '',
+      listing: [],
+      disabledButton: false
+    };
+  },
+  watch: {
+    directory(val) {
+      if (val == '') {
+        this.disabledButton = true
+        this.listing = []
+      } else {
+        this.disabledButton = false
+      }
+    }
+  },
+  methods: {
+    getDirectoryListing() {
+      axios.get('http://getlocalfiles.local/api/get-directory-listing', { params: { directory: this.directory } })
+        .then(response => {
+          this.listing = response.data.listing;
+          this.disabledButton = true
+        })
+        .catch(error => {
+          console.error('Error fetching directory listing', error);
+        });
+    },
+    enabledButton() {
+      this.disabledButton = false;
+    }
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// import { BootstrapVue, BFormInput } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
 Vue.config.productionTip = false
+
+// // Make BootstrapVue available throughout your project
+// Vue.use(BootstrapVue)
+// Vue.use(BFormInput)
 
 new Vue({
   render: h => h(App),
